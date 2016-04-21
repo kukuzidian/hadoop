51ebafe037eb9991c07858ac7cac81d9ae02afbb
cenyuhai <cenyuhai@didichuxing.com>
2016-04-21 20:19:52 +0800

    更新部署脚本

--- publish-to-nexux.sh
+++ publish-to-nexux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PUBLISH_PROFILES="-Pnative -DskipTests -Psources -Pjavadoc"
+PUBLISH_PROFILES="-Pnative -DskipTests"
 
 echo "Publishing Hadoop to nexus"
 mvn clean deploy $PUBLISH_PROFILES
