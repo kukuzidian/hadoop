a5d62bf59ce5ff68df52fef8b8838b9ff8b5c4c8
cenyuhai <cenyuhai@didichuxing.com>
2016-04-21 20:17:01 +0800

    更新部署脚本

--- publish-to-nexux.sh
+++ publish-to-nexux.sh
@@ -2,9 +2,5 @@
 
 PUBLISH_PROFILES="-Pnative -DskipTests -Psources -Pjavadoc"
 
-echo "Replacing groupID with com.didichuxing.hadoop"
-find . -name pom.xml | \
-  xargs -I {} sed -i -e "s/org.apache.hadoop/com.didichuxing.hadoop/g" {}
-
 echo "Publishing Hadoop to nexus"
-mvn clean deploy -Dgpg.passphrase=-Dgpg.skip $PUBLISH_PROFILES
+mvn clean deploy $PUBLISH_PROFILES
