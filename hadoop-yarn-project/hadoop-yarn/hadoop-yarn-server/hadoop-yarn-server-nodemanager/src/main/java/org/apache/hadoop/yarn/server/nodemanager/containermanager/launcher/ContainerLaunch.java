c1aecd9a2024c53e891975ea9c0d601b8f57ccbb
root <yuyi@didichuxing.com>
2016-08-16 13:45:25 +0800

    Revert "Revert "add log for signal container""
    This reverts commit 855d8988e2c8e194684f3b1df647f03599a7aca9.

--- hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/launcher/ContainerLaunch.java
+++ hadoop-yarn-project/hadoop-yarn/hadoop-yarn-server/hadoop-yarn-server-nodemanager/src/main/java/org/apache/hadoop/yarn/server/nodemanager/containermanager/launcher/ContainerLaunch.java
@@ -408,7 +408,7 @@ public class ContainerLaunch implements Callable<Integer> {
       // kill process
       if (processId != null) {
         String user = container.getUser();
-        LOG.debug("Sending signal to pid " + processId
+        LOG.info("Sending signal to pid " + processId
             + " as user " + user
             + " for container " + containerIdStr);
 
