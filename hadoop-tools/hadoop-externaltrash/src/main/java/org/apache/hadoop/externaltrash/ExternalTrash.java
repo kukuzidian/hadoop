e4feef9df63ec015a1a97a66520d76a4a4c3aab9
liyinkevin <liyinkevin@didichuxing.com>
2016-11-15 22:16:09 +0800

    external trash as a service.

--- hadoop-tools/hadoop-externaltrash/src/main/java/org/apache/hadoop/externaltrash/ExternalTrash.java
+++ hadoop-tools/hadoop-externaltrash/src/main/java/org/apache/hadoop/externaltrash/ExternalTrash.java
@@ -10,14 +10,16 @@ import java.util.Map;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.fs.*;
+import org.apache.hadoop.util.StringUtils;
+import org.apache.hadoop.util.Time;
 
-import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.FS_TRASH_INTERVAL_KEY;
+import static org.apache.hadoop.fs.CommonConfigurationKeysPublic.*;
 
 /**
  * A external trash utility to create and delete checkpoints of different user's trash
  * with different interval.
  */
-public class ExternalTrash {
+public class ExternalTrash implements Runnable {
 
     private static final Log LOG = LogFactory.getLog(ExternalTrash.class);
 
@@ -25,7 +27,12 @@ public class ExternalTrash {
     private static final String CONF_PREFIX = "fs.user.";
     private static final String CONF_SUFFIX = ".trash.interval";
 
+    private static final int MSECS_PER_MINUTE = 60*1000;
+
+    private final FileSystem fs;
     private final Configuration conf;
+
+    private long emptierInterval;
 
     private Map<String, Float> user2Interval = new HashMap<>();
 
@@ -34,18 +41,37 @@ public class ExternalTrash {
         System.setProperty("HADOOP_USER_NAME", "hadoop");
     }
 
-    public ExternalTrash(Configuration conf) {
+    public ExternalTrash(Configuration conf) throws IOException {
         this.conf = conf;
+        fs = FileSystem.get(this.conf);
 
-        init();
+        long deletionInterval = (long)(conf.getFloat(
+                FS_TRASH_INTERVAL_KEY, FS_TRASH_INTERVAL_DEFAULT)
+                * MSECS_PER_MINUTE);
+        this.emptierInterval = (long)(conf.getFloat(
+                FS_TRASH_CHECKPOINT_INTERVAL_KEY, FS_TRASH_CHECKPOINT_INTERVAL_DEFAULT)
+                * MSECS_PER_MINUTE);
+        LOG.info("External Trash configuration: Deletion interval = " +
+                (deletionInterval / MSECS_PER_MINUTE) + " minutes, Emptier interval = " +
+                (this.emptierInterval / MSECS_PER_MINUTE) + " minutes.");
+
+        if (this.emptierInterval <= 0 || this.emptierInterval > deletionInterval) {
+            throw new IllegalArgumentException(FS_TRASH_CHECKPOINT_INTERVAL_KEY + "'s value is illegal.");
+        }
     }
 
     /**
      * Loop configurations and collect username and its trash-interval.
      */
-    private void init() {
+    private void reloadConfig() {
 
-        for (Map.Entry<String, String> entry : this.conf) {
+        this.user2Interval.clear();
+
+        Configuration trashConf = new Configuration(false);
+        trashConf.addResource(CONF_FILENAME);
+
+        // load user' trash interval config
+        for (Map.Entry<String, String> entry : trashConf) {
             if (!entry.getKey().startsWith(CONF_PREFIX) ||
                     !entry.getKey().endsWith(CONF_SUFFIX)) {
                 continue;
@@ -66,49 +92,90 @@ public class ExternalTrash {
     /**
      * Scan home's parent directory and process user's trash one by one.
      */
-    public void run() throws IOException {
+    @Override
+    public void run() {
 
-        try(FileSystem fs = FileSystem.get(this.conf)){
-            Path homesParent = fs.getHomeDirectory().getParent();
+        Path homesParent = fs.getHomeDirectory().getParent();
 
-            FileStatus[] homes = fs.listStatus(homesParent);         // list all home dirs
-
-            for (FileStatus home : homes) {         // dump each trash
-                if (!home.isDirectory())
-                    continue;
-
-                Configuration copyConf = new Configuration(this.conf);
-                String user = home.getPath().getName();
-                if (this.user2Interval.containsKey(user)) {
-                    copyConf.setFloat(FS_TRASH_INTERVAL_KEY, this.user2Interval.get(user));
-                }
-
-                try {
-                    TrashPolicy trash = TrashPolicy.getInstance(copyConf, fs, home.getPath());
-                    trash.deleteCheckpoint();
-                    trash.createCheckpoint();
-                } catch (IOException e) {
-                    LOG.warn("Trash caught: " + e + ". Skipping " + home.getPath() + ".");
-                }
+        while (true) {
+            long now = Time.now();
+            long end = ceiling(now, emptierInterval);
+            try {                                     // sleep for interval
+                Thread.sleep(end - now);
+            } catch (InterruptedException e) {
+                break;                                  // exit on interrupt
             }
 
+            // reload configuration every time, so we can reconfiguration without restart daemon.
+            reloadConfig();
+
+            try {
+                now = Time.now();
+                if (now >= end) {
+
+                    FileStatus[] homes = null;
+                    try {
+                        homes = fs.listStatus(homesParent);         // list all home dirs
+                    } catch (IOException e) {
+                        LOG.warn("Trash can't list homes: "+e+" Sleeping.");
+                        continue;
+                    }
+
+                    for (FileStatus home : homes) {         // dump each trash
+                        if (!home.isDirectory())
+                            continue;
+
+                        // update trash interval if necessary
+                        Configuration copyConf = new Configuration(this.conf);
+                        String user = home.getPath().getName();
+                        if (this.user2Interval.containsKey(user)) {
+                            copyConf.setFloat(FS_TRASH_INTERVAL_KEY, this.user2Interval.get(user));
+                        }
+
+                        try {
+                            TrashPolicy trash = TrashPolicy.getInstance(copyConf, fs, home.getPath());
+                            trash.deleteCheckpoint();
+                            trash.createCheckpoint();
+                        } catch (IOException e) {
+                            LOG.warn("ExternalTrash caught: "+e+". Skipping "+home.getPath()+".");
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                LOG.warn("RuntimeException during ExternalTrash.run(): ", e);
+            }
         }
+        try {
+            fs.close();
+        } catch(IOException e) {
+            LOG.warn("ExternalTrash cannot close FileSystem: ", e);
+        }
+
     }
 
+    private long ceiling(long time, long interval) {
+        return floor(time, interval) + interval;
+    }
+    private long floor(long time, long interval) {
+        return (time / interval) * interval;
+    }
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        StringUtils.startupShutdownMessage(ExternalTrash.class, args, LOG);
 
         if (ExternalTrash.class.getClassLoader().getResource(CONF_FILENAME) == null) {
             LOG.error("can't find " + CONF_FILENAME);
             return;
         }
 
-        Configuration conf = new Configuration();
-        conf.addResource(CONF_FILENAME);
+        ExternalTrash externalTrash = new ExternalTrash(new Configuration());
 
-        ExternalTrash externalTrash = new ExternalTrash(conf);
+        Thread externalTrashThread = new Thread(externalTrash, "External Trash Thread");
+        externalTrashThread.setDaemon(true);
+        externalTrashThread.start();
 
-        externalTrash.run();
+        externalTrashThread.join();
     }
 
 
